a2cce968d3fa5d05f315aa2ed7bde79c78978176
mo107ok <113059563+mo107ok@users.noreply.github.com>
2022-09-17 13:10:12 +0300

    feat; rasminka.cpp

---
+++ 03_cpp/intro/rasminka.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream> 
+#include <string> 
+
+int main()
+{
+	std::cout << "Введите имя ";
+	std::string name;
+
+	std::getline(std::cin, name);
+
+	std::cout << "Здравствуйте," << name << '!';
+
+	std::cin.get();
+	return 0;
+}
