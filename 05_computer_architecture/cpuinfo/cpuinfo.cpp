50af7faf63faf34a96124fd39c8ae684c84f6a17
mo107ok <molotkov.ad@edu.spbstu.ru>
2022-11-25 20:00:48 +0300

    fix: cpuinfo.cpp updated

--- 05_computer_architecture/cpuinfo/cpuinfo.cpp
+++ 05_computer_architecture/cpuinfo/cpuinfo.cpp
@@ -1,14 +1,17 @@
-#include <chrono>
-#include <fstream>
 #include <iostream>
+#include <fstream>
+#include <chrono>
 #include <thread>
 
-void get_cpuinfo(unsigned long long int fields[]) {
+void get_cpuinfo(unsigned long long int fields[])
+{
     char line[3];
     std::ifstream in("/proc/stat", std::ios_base::in);
     in >> line;
-    for (int i = 0; i != 10; ++i) {
-        if (in.is_open()) {
+    for (int i = 0; i != 10; ++i)
+    {
+        if (in.is_open())
+        {
             in >> fields[i];
         }
         else {
@@ -17,30 +20,35 @@ void get_cpuinfo(unsigned long long int fields[]) {
     }
 }
 
-int main() {
-    unsigned long long int fields[10], total_tick, total_tick_old, idle, idle_old,
-        del_total_tick, del_idle;
+int main()
+{
+    unsigned long long int fields[10], total_tick, total_tick_old, idle, idle_old, del_total_tick, del_idle;
+
     double percent_usage;
 
     get_cpuinfo(fields);
 
     total_tick = 0;
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 10; i++)
+    {
         total_tick += fields[i];
     }
-    idle = fields[3]; /* idle ticks index */
+    idle = fields[3];
 
-    while (true) {
-        std::chrono::milliseconds timespan(1000); // or whatever
+    while (true)
+    {
+        std::chrono::milliseconds timespan(1000);
         std::this_thread::sleep_for(timespan);
 
         total_tick_old = total_tick;
+
         idle_old = idle;
 
         get_cpuinfo(fields);
 
         total_tick = 0;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 10; i++)
+        {
             total_tick += fields[i];
         }
         idle = fields[3];
@@ -48,10 +56,12 @@ int main() {
         del_total_tick = total_tick - total_tick_old;
         del_idle = idle - idle_old;
 
-        percent_usage =
-            ((del_total_tick - del_idle) / (double)del_total_tick) * 100;
+        percent_usage = ((del_total_tick - del_idle) / (double)del_total_tick) * 100;
         printf("Total CPU Usage: %3.2lf%%\n", percent_usage);
-    }
 
+
+
+
+    }
     return 0;
 }
