cd6f9d646e10cf61d3cc49ef67aa929a418f0fb3
molotkov.ad@edu.spbstu.ru <molotkov.ad@edu.spbstu.ru>
2022-12-05 19:02:47 +0300

    feat: README.md created

--- 06_pointers_references/matmul/matmul.cpp
+++ 06_pointers_references/matmul/matmul.cpp
@@ -1,45 +1,140 @@
-usage: git [-v | --version] [-h | --help] [-C <path>] [-c <name>=<value>]
-           [--exec-path[=<path>]] [--html-path] [--man-path] [--info-path]
-           [-p | --paginate | -P | --no-pager] [--no-replace-objects] [--bare]
-           [--git-dir=<path>] [--work-tree=<path>] [--namespace=<name>]
-           [--super-prefix=<path>] [--config-env=<name>=<envvar>]
-           <command> [<args>]
 
-These are common Git commands used in various situations:
 
-start a working area (see also: git help tutorial)
-   clone     Clone a repository into a new directory
-   init      Create an empty Git repository or reinitialize an existing one
+#include <iostream>
+#include<cassert>
 
-work on the current change (see also: git help everyday)
-   add       Add file contents to the index
-   mv        Move or rename a file, a directory, or a symlink
-   restore   Restore working tree files
-   rm        Remove files from the working tree and from the index
+using namespace std;
 
-examine the history and state (see also: git help revisions)
-   bisect    Use binary search to find the commit that introduced a bug
-   diff      Show changes between commits, commit and working tree, etc
-   grep      Print lines matching a pattern
-   log       Show commit logs
-   show      Show various types of objects
-   status    Show the working tree status
 
-grow, mark and tweak your common history
-   branch    List, create, or delete branches
-   commit    Record changes to the repository
-   merge     Join two or more development histories together
-   rebase    Reapply commits on top of another base tip
-   reset     Reset current HEAD to the specified state
-   switch    Switch branches
-   tag       Create, list, delete or verify a tag object signed with GPG
+int** getMatrix(int m, int n) {
+	int** arr = new int* [m];
 
-collaborate (see also: git help workflows)
-   fetch     Download objects and refs from another repository
-   pull      Fetch from and integrate with another repository or a local branch
-   push      Update remote refs along with associated objects
+	for (int i = 0; i < m; i++) {
+		arr[i] = new int[n];
+	}
 
-'git help -a' and 'git help -g' list available subcommands and some
-concept guides. See 'git help <command>' or 'git help <concept>'
-to read about a specific subcommand or concept.
-See 'git help git' for an overview of the system.
+	return arr;
+}
+
+
+void fillMatrix(int** matrix, int m, int n) {
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			matrix[i][j] = rand() % 19 - 9;
+		}
+	}
+}
+
+
+void printMatrix(int** matrix, int m, int n, int widthCharSize) {
+	for (int i = 0; i < m; i++) {
+		for (int j = 0; j < n; j++) {
+			cout.width(widthCharSize);
+			cout << matrix[i][j];
+		}
+		cout << endl;
+	}
+}
+
+
+void deleteMatrix(int** matrix, int m) {
+	for (int i = 0; i < m; i++) {
+		delete matrix[i];
+	}
+	delete[] matrix;
+}
+
+
+void matmul(int** a, int m_a, int n_a, int** b, int m_b, int n_b, int** c) {
+	for (int i = 0; i < m_a; i++)
+	{
+		for (int j = 0; j < n_b; j++)
+		{
+			c[i][j] = 0;
+			for (int k = 0; k < n_a; k++) {
+				c[i][j] += a[i][k] * b[k][j];
+			}
+		}
+	}
+}
+
+
+int getMatMulValAtIndices(int** a, int** b, int a_i, int b_i, int equalSide) {
+	int mulValue = 0;
+
+	for (int i = 0; i < a_i; i++)
+	{
+		for (int j = 0; j < b_i; j++)
+		{
+			for (int k = 0; k < equalSide; k++) {
+				mulValue += a[i][k] * b[k][j];
+			}
+		}
+	}
+
+	return mulValue;
+}
+
+
+bool mateq(int** a, int m_a, int n_a, int** b, int m_b, int n_b) {
+	if (m_a != m_b || n_a != n_b) {
+		return false;
+	}
+
+	for (int i = 0; i < m_a; i++) {
+		for (int j = 0; j < n_a; j++) {
+			if (a[i][j] != b[i][j]) {
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+
+bool isMatricesCanBeMultiplied(int n_a, int m_b) {
+	return n_a == m_b;
+}
+
+
+int main()
+{
+	int m_a = 8;
+	int n_a = 10;
+	int m_b = 10;
+	int n_b = 7;
+
+	srand(time(0));
+
+	int** a = getMatrix(m_a, n_a);
+	int** b = getMatrix(m_b, n_b);
+
+	fillMatrix(a, m_a, n_a);
+	fillMatrix(b, m_b, n_b);
+
+	printMatrix(a, m_a, n_a, 3);
+
+	cout << endl << endl;
+
+	printMatrix(b, m_b, n_b, 3);
+
+	int** c = getMatrix(m_a, n_b);
+
+	if (!isMatricesCanBeMultiplied(n_a, m_b)) {
+		cout << endl << "Can't multiply specified matrices" << endl;
+		return 0;
+	}
+
+	matmul(a, m_a, n_a, b, m_b, n_b, c);
+
+	cout << endl << endl;
+
+	printMatrix(c, m_a, n_b, 5);
+
+	cout << endl << endl;
+
+	mateq(a, m_a, n_a, b, m_b, n_b) ? cout << "Matrices are equal" : cout << "Matrices are not equal" << endl;
+
+	assert(getMatMulValAtIndices(a, b, 1, 1, n_a) == c[0][0]);
+}
--- 06_pointers_references/matmul/matmul.h
+++ 06_pointers_references/matmul/matmul.h
@@ -1,45 +1,45 @@
-usage: git [-v | --version] [-h | --help] [-C <path>] [-c <name>=<value>]
-           [--exec-path[=<path>]] [--html-path] [--man-path] [--info-path]
-           [-p | --paginate | -P | --no-pager] [--no-replace-objects] [--bare]
-           [--git-dir=<path>] [--work-tree=<path>] [--namespace=<name>]
-           [--super-prefix=<path>] [--config-env=<name>=<envvar>]
-           <command> [<args>]
+#include <iostream>
 
-These are common Git commands used in various situations:
 
-start a working area (see also: git help tutorial)
-   clone     Clone a repository into a new directory
-   init      Create an empty Git repository or reinitialize an existing one
+using namespace std;
 
-work on the current change (see also: git help everyday)
-   add       Add file contents to the index
-   mv        Move or rename a file, a directory, or a symlink
-   restore   Restore working tree files
-   rm        Remove files from the working tree and from the index
 
-examine the history and state (see also: git help revisions)
-   bisect    Use binary search to find the commit that introduced a bug
-   diff      Show changes between commits, commit and working tree, etc
-   grep      Print lines matching a pattern
-   log       Show commit logs
-   show      Show various types of objects
-   status    Show the working tree status
+int** arr_create(int rows, int cols) {
+    int** arr = new int* [rows];
+    for (int i = 0; i < rows; i++) arr[i] = new int[cols];
 
-grow, mark and tweak your common history
-   branch    List, create, or delete branches
-   commit    Record changes to the repository
-   merge     Join two or more development histories together
-   rebase    Reapply commits on top of another base tip
-   reset     Reset current HEAD to the specified state
-   switch    Switch branches
-   tag       Create, list, delete or verify a tag object signed with GPG
+    return arr;
+}
 
-collaborate (see also: git help workflows)
-   fetch     Download objects and refs from another repository
-   pull      Fetch from and integrate with another repository or a local branch
-   push      Update remote refs along with associated objects
 
-'git help -a' and 'git help -g' list available subcommands and some
-concept guides. See 'git help <command>' or 'git help <concept>'
-to read about a specific subcommand or concept.
-See 'git help git' for an overview of the system.
+void fill_array(int** arr, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        cout << "row: " << i + 1 << endl;
+
+        for (int j = 0; j < cols; j++) cin >> arr[i][j];
+    }
+    cout << endl;
+}
+
+
+void print_array(int** arr, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) cout << arr[i][j] << " ";
+        cout << endl << endl;
+    }
+}
+
+
+void matmul(int** a, int r_a, int c_a, int** b, int r_b, int c_b, int** c) {
+    for (int i = 0; i < r_a; i++) {
+        for (int j = 0; j < c_b; j++) {
+            c[i][j] = 0;
+            for (int k = 0; k < r_b; k++) c[i][j] += a[i][k] * b[k][j];
+        }
+    }
+}
+
+
+void delete_arr(int** a, int m) {
+    for (int i = 0; i < m; i++) delete[] a[i];
+}
